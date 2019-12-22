d5840fb92c793a01126d464d69b319243188edd1
silaSaaraoglu <sila.saraoglu@gamil.com>
2019-12-22 21:45:56 +0300

    patientdetailes ekledim

--- src/ui/receptionist/controllers/patientDetailsController.java
+++ src/ui/receptionist/controllers/patientDetailsController.java
@@ -1,4 +1,45 @@
 package ui.receptionist.controllers;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+
+
 public class patientDetailsController {
-}
+
+    public patientDetailsController(){}
+
+    @FXML
+    private Label detailedNameLabel;
+    @FXML
+    private Label detailedIDLabel;
+    @FXML
+    private Label detailedGenderLabel;
+    @FXML
+    private Label detailedBloodTypeLabel;
+    @FXML
+    private Label detailedSurnameLabel;
+    @FXML
+    private Label detailedBirthDateLabel;
+    @FXML
+    private Label detailedAddressLabel;
+    @FXML
+    private Label detailedCityLabel;
+    @FXML
+    private Label detailedPhoneNumber;
+    @FXML
+    private Label detailedInsurance;
+    @FXML
+    private Label detailedEmergencyContactName;
+    @FXML
+    private Label detailedEmergencyContactSurname;
+    @FXML
+    private Label detailedEmergencyContactPhoneNumber;
+    @FXML
+    private Button exitButton;
+
+
+    }
