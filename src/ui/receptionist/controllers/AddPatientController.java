5cad9f2070c8f6fca8fe1edd299e6620ad01a18b
Mert Yıldırım <yusuf.yildirim@ug.bilkent.edu.tr>
2019-12-22 22:19:06 +0300

    Parser for birth date

--- src/ui/receptionist/controllers/AddPatientController.java
+++ src/ui/receptionist/controllers/AddPatientController.java
@@ -145,6 +145,7 @@ private Button saveButton;
         String year = date.substring(0,4);
         String month = date.substring(5,7);
         String day = date.substring(8,10);
+
         return day+"/"+ month+"/"+ year;
     }
 
