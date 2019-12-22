ba85d0dba384afc47fc7afd06666a53959f0101e
yarenyy <yaren.yilmaz@ug.bilkent.edu.tr>
2019-12-22 16:46:53 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/classes/Receptionist.java

--- src/classes/Patient.java
+++ src/classes/Patient.java
@@ -14,9 +14,9 @@ public class Patient
     private ArrayList<Doctor> doctors;
     private LocalDate birthDate;
 
-    public Patient(String userName, String password, String name, String bloodType, String room, String emergencyContact, String insurance, String citizenshipID, String address, LocalDate birthDate, int age, char gender) {
-        this.userName = userName;
-        this.password = password;
+    public Patient(String name, String bloodType, String room, String emergencyContact, String insurance, String citizenshipID, String address, LocalDate birthDate, int age, char gender)
+    {
+
         this.name = name;
         this.bloodType = bloodType;
         this.room = room;
@@ -33,9 +33,6 @@ public class Patient
         this.birthDate = birthDate;
     }
 
-    public String getUserName() {
-        return userName;
-    }
 
     public ArrayList<String> getOldPrescriptions() {
         return oldPrescriptions;
@@ -53,9 +50,6 @@ public class Patient
         return appointments;
     }
 
-    public String getPassword() {
-        return password;
-    }
 
     public String getName() {
         return name;
@@ -95,14 +89,6 @@ public class Patient
 
     public LocalDate getBirthDate() {
         return birthDate;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public void setRoom(String room) {
