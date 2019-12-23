c4777872902a86b06387b7b369381a80244b1a7f
silaSaaraoglu <sila.saraoglu@gamil.com>
2019-12-23 14:37:51 +0300

    add appointment save buttonu commentli

--- src/ui/receptionist/controllers/AddAppointmentController.java
+++ src/ui/receptionist/controllers/AddAppointmentController.java
@@ -1,4 +1,41 @@
 package ui.receptionist.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import java.awt.event.*;
+
 public class AddAppointmentController {
+
+    public AddAppointmentController(){}
+
+    @FXML
+    TextField patientName;
+    @FXML
+    TextField patientSurname;
+    @FXML
+    TextField doctorName;
+    @FXML
+    TextField doctorSurname;
+    @FXML
+    TextField appointmentDate;
+    @FXML
+    TextField appointmentTime;
+    @FXML
+    Button saveAppointmentButton;
+
+   /** private void saveAppointment(ActionEvent){
+
+        /// if doctor is available
+        if ( classes.Receptionist.addAppointment(patient,doctor,cost,time) > 0 )
+        {
+
+        }
+
+        else
+        {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Doctor is unavailable\n Please select another doctor",ButtonType.OK);
+            alert.showAndWait();
+        }
+
+    }**/
 }
