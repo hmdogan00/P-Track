e6b6ebc6fcfe954f1eaf81f93c7ee6a526298e0a
Mert Yıldırım <yusuf.yildirim@ug.bilkent.edu.tr>
2019-12-23 11:41:22 +0300

    Parser for birth date

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,22 +1,22 @@
 package database;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Database {
     public static void main(String[] args) throws SQLException {
-        addPatient("Aynur Dayanık", 12312312, "Female","A+", "17/09/1960", "ankara",
-                01237612736, "Private", "Mustafa Can Çavdar", 0123671723);
-
-
         }
 
-        public static Connection connection() throws SQLException {
-            String url = "jdbc:mysql://db-mysql-fra1-50385-do-user-6846837-0.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED";
-            String user = "doadmin";
-            String password = "ozhcj9y9h18feuqk";
-            Connection myConn = DriverManager.getConnection(url, user, password);
-            return myConn;
+    //Connection method including connection details
+    public static Connection connection() throws SQLException {
+        String url = "jdbc:mysql://db-mysql-fra1-50385-do-user-6846837-0.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED";
+        String user = "doadmin";
+        String password = "ozhcj9y9h18feuqk";
+        Connection myConn = DriverManager.getConnection(url, user, password);
+        return myConn;
     }
+
+    //Test Method
     public static void test() throws SQLException {
         Connection myConn = connection();
         Statement myStmt = myConn.createStatement();
@@ -28,6 +28,7 @@ public class Database {
             System.out.println(rs.getString("name"));
         }
     }
+    //Method to add new patient to database
     public static void addPatient(String name, int Id, String sex, String blood_type, String birth_date, String address,
                                   int phoneNumber, String insurance, String emergency_name, int emergency_number ) throws SQLException{
         Connection myConn = connection();
@@ -46,5 +47,65 @@ public class Database {
 
         int rowsAffected = myStmt.executeUpdate();
     }
+
+    //Method to find patient key number on table
+    public static void patientKey (String p_name) throws SQLException {
+        Connection myConn = connection();
+        String sql = "SELECT patient_id FROM patient WHERE name = ?";
+        PreparedStatement myStmt = myConn.prepareStatement(sql);
+        myStmt.setString(1,p_name);
+        int rowsAfected = myStmt.executeUpdate();
+    }
+
+    //Method to update information of patient
+    public static void updatePatient(int p_key, String p_address, int patient_phone, String insurance, String emergency_name, int emergency_number) throws SQLException{
+        Connection myConn = connection();
+        String sql ="UPDATE patient SET patient_address = ? , patient_phoneNumber = ?, insurance = ?, emergency_name = ?, emergency_number = ? WHERE patient_id = ? ";
+
+        PreparedStatement myStmt = myConn.prepareStatement(sql);
+        myStmt.setString(1, p_address);
+        myStmt.setString(2,"" + patient_phone);
+        myStmt.setString(3, insurance);
+        myStmt.setString(4, emergency_name);
+        myStmt.setString(5,"" + emergency_number);
+        myStmt.setString(6,"" + p_key);
+        int rowsAffected = myStmt.executeUpdate();
+    }
+
+    //Patient info with specific key value
+    public static ArrayList<String> patientDetails(int p_key) throws SQLException {
+        String name, id, gender, blood, birth_date, address, insurance, phone_number, emergency_name, emergency_number;
+
+        ArrayList<String> patient_info = new ArrayList<>();
+
+        Connection myConn = connection();
+        String sql = " SELECT * FROM patient WHERE patient_id = " + p_key;
+        Statement myStmt = myConn.createStatement();
+        ResultSet rs = myStmt.executeQuery(sql);
+        name = rs.getString("name");
+        id = rs.getString("citizenship_id");
+        gender = rs.getString("gender");
+        blood = rs.getString("blood_type");
+        birth_date = rs.getString("birth_date");
+        address = rs.getString("patient_address");
+        insurance = rs.getString("insurance");
+        phone_number = rs.getString("patient_phoneNumber");
+        emergency_name = rs.getString("emergency_name");
+        emergency_number = rs.getString("emergency_number");
+
+        patient_info.add(name);
+        patient_info.add(id);
+        patient_info.add(gender);
+        patient_info.add(blood);
+        patient_info.add(birth_date);
+        patient_info.add(address);
+        patient_info.add(insurance);
+        patient_info.add(phone_number);
+        patient_info.add(emergency_name);
+        patient_info.add(emergency_number);
+
+        return patient_info;
+    }
+
 
 }
