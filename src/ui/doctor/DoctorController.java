ca82bb0886549be47cb92e0069df2f36b7ebc194
Mert Yıldırım <yusuf.yildirim@ug.bilkent.edu.tr>
2019-12-25 19:16:15 +0300

    Database features added

--- src/ui/doctor/DoctorController.java
+++ src/ui/doctor/DoctorController.java
@@ -111,7 +111,7 @@ public class DoctorController extends MasterController implements Initializable 
         ObservableList<UpcomingTable> obList3 = FXCollections.observableArrayList();
         try {
             Connection con = Database.myConn;
-            ResultSet rs = con.createStatement().executeQuery("SELECT patient.`name`, appointment.`date`, appointment.`time`, patient.`patient_phoneNumber` FROM patient, appointment, doctor WHERE appointment.`doctor_id` = doctor.`doctor_id` AND appointment.`patient_id` = patient.`patient_id` AND doctor.`doctor_id`= '" + doctorId + "' AND appointment.`date` >= '"+ Database.date() +"' ORDER BY `date` DESC AND patient.'name' LIKE '%" + patientNameFilter + "%' ");
+            ResultSet rs = con.createStatement().executeQuery("SELECT patient.`name`, appointment.`date`, appointment.`time`, patient.`patient_phoneNumber` FROM patient, appointment, doctor WHERE appointment.`doctor_id` = doctor.`doctor_id` AND appointment.`patient_id` = patient.`patient_id` AND doctor.`doctor_id`= '" + doctorId + "' AND appointment.`date` >= '"+ Database.date() +"' AND patient.`name` LIKE '%" + patientNameFilter + "%' ORDER BY `date` DESC ");
 
             while (rs.next()) {
                 obList3.add(new UpcomingTable(rs.getString("name"), rs.getString("date"),
