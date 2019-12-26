929a3ab503000a6eacc70a8a4a9f7371f3c81bec
Mert Yıldırım <yusuf.yildirim@ug.bilkent.edu.tr>
2019-12-26 11:24:47 +0300

    Database features added

---
+++ src/ui/receptionist/DocAppointmentTable.java
@@ -0,0 +1,45 @@
+package ui.receptionist;
+
+public class DocAppointmentTable {
+    String name, date, time;
+    String phoneNo;
+
+    public DocAppointmentTable(String name, String phoneNo, String date, String time){
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
