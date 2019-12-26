d35656e19fdfc0ccf6c546ce28f7f777b1672570
borafkazanci <fatih.kazanci@ug.bilkent.edu.tr>
2019-12-26 15:00:18 +0300

    doctor logout

--- src/ui/receptionist/controllers/AddAppointmentController.java
+++ src/ui/receptionist/controllers/AddAppointmentController.java
@@ -42,10 +42,6 @@ public class AddAppointmentController extends MasterController implements Initia
     @FXML
     private ChoiceBox<String> choiceBox;
 
-    //error labels
-    @FXML
-    private Label errorLabelAdd;
-
     //variables
     private String patientdbId;
 
